3350f50e0826f1aace4da126484c433a165b1d4a
Songyeongman <thsrudaks6334@naver.com>
2023-11-08 19:46:27 +0900

    약수의 합 구하는 방법(제곱근 사용)

---
+++ programmers/src/main/java/org/example/LV1/약수의_합.java
@@ -0,0 +1,22 @@
+package org.example.LV1;
+
+public class 약수의_합 {
+    /*
+    * 정수 n을 입력받아 n의 약수를 모두 더한 값을 리턴하는 함수, solution을 완성해주세요.
+    * */
+    public static void main(String[] args) {
+        System.out.println(solution(5));
+    }
+
+    public static int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i < Math.sqrt(n); i++) {
+            if(n % i == 0){
+                answer += i;
+                if(i != n / i) answer += n / i;
+            }
+        }
+        return answer;
+    }
+
+}
